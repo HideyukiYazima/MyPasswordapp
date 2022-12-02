091d5e62f8f6505262ee16a56acffa00e65d4fe2
Yazima <yazima04200@gmail.com>
2022-12-02 20:34:13 +0900

    passランダム生成実装

--- main.js
+++ main.js
@@ -2,10 +2,30 @@
 
 {
   const slider = document.getElementById('slider');
+  const btn = document.getElementById('btn');
 
   slider.addEventListener('input', () => {
     const passwordLength = document.getElementById('password-length');
 
     passwordLength.textContent = slider.vanlue;
+  });
+
+  btn.addEventListener('click', () => {
+    const result = document.getElementById('result');
+    const numbersCheckbox = document.getElementById('numbers-checkbox');
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    let password = '';
+    const seed = letters + letters.toUpperCase();
+
+    if (numbersCheckbox.checked) {
+      seed += numbers;
+    }
+
+    for (let i = 0; i < slider.value; i++) {
+      password += seed[Math.floor(Math.random() * seed.length)];
+    }
+
+    result.textContent = password;
   });
 }
